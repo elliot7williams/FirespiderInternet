43d2c3476cb09923533cb71e9b9087652c5e72ec
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 19:33:06 -0400

    Add Firespider Internet/My Project/AssemblyInfo.vb

---
+++ Firespider Internet/My Project/AssemblyInfo.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+'General Information about an assembly is controlled through the following 
+'set of attributes. Change these attribute values to modify the information
+'associated with an assembly.
+'Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Firespider Internet 17")>
+<Assembly: AssemblyDescription("Firespider Internet 17")>
+<Assembly: AssemblyCompany("Oasis Mobile inc.")>
+<Assembly: AssemblyProduct("Firespider Internet 17")>
+<Assembly: AssemblyCopyright("Copyright © Oasis Mobile inc. 2017")>
+<Assembly: AssemblyTrademark("Oasis Mobile inc. 2017")>
+<Assembly: ComVisible(False)> 
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cd64799c-3963-4ac0-9595-612a2179f7b6")>
+'Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+'You can specify all the values or you can default the Build and Revision Numbers 
+'by using the '*' as shown below:
+'<Assembly: AssemblyVersion("1.0.*")>
+<Assembly: AssemblyVersion("17.0.0.0")>
+<Assembly: AssemblyFileVersion("17.0.0.0")>
+<Assembly: NeutralResourcesLanguageAttribute("en-CA")> 
